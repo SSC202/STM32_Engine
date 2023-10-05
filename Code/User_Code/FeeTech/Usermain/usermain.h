e24f91dc87b7af5386a0e277b26988227b15838c
SSC202 <e22750706642022@163.com>
2023-10-05 13:17:40 +0800

    飞特舵机 HAL库例程（2023.10.5）

---
+++ Code/User_Code/FeeTech/Usermain/usermain.h
@@ -0,0 +1,20 @@
+#ifndef __Usermain_H
+#define __Usermain_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+#include "usart.h"
+#include <stdio.h>
+
+void User_main(void);
+
+#ifdef __cplusplus
+};
+#endif
+
+#include "FeeTech.hpp"
+
+#endif
