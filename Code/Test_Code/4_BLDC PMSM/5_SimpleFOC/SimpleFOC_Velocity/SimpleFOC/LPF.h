522ad94b72cba9bf25ffcf6740bf8dd0642c63e3
SSC202 <e22750706642022@163.com>
2024-03-07 21:47:57 +0800

    Update

--- Code/Test_Code/4_BLDC PMSM/5_SimpleFOC/SimpleFOC_Velocity/SimpleFOC/LPF.h
+++
@@ -1,6 +0,0 @@
-#ifndef __LPF_H
-#define __LPF_H
-
-float LPF_velocity(float x);
-
-#endif
--- Code/Test_Code/4_BLDC PMSM/5_SimpleFOC/SimpleFOC_Velocity/SimpleFOC/pid.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PID_H
-#define __PID_H
-
-void PID_init(void);
-float PID_velocity(float error);
-float PID_angle(float error);
-
-#endif
